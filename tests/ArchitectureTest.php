bebe7a0fdb078fcdd64c2f0184debc8939d4ef4b
calavera <filomontani@icloud.com>
2024-11-05 10:46:17 +0100

    fix: compatibility stuff

--- tests/ArchitectureTest.php
+++ tests/ArchitectureTest.php
@@ -2,9 +2,11 @@
 
 declare(strict_types=1);
 
+if(phpversion() >= '8.1') {
 arch()->preset()->php();
 
 arch()
     ->expect('Datomatic\EnumCollections')
     ->toUseStrictTypes()
     ->not->toUse(['die', 'dd', 'dump']);
+}
